857aa559bd6668013d324c95e01f89a2e0941efd
leewa <leewa@DESKTOP-5OCQG05>
2017-02-23 16:51:50 +0800

    Add project files.

---
+++ IoTHardwareTest/Modules/MainFrame/Converter/SettingNaviConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace IoTHardwareTest.Modules.MainFrame.Converter
+{
+    public class SettingNaviConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value is SettingPage.View.SettingPage ? 0 : -1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IoTHardwareTest/Tools/StringResHelpler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml;
+
+namespace IoTHardwareTest.Tools
+{
+    public class StringResHelpler
+    {
+        private static ResourceLoader CurrentResourceLoader
+        {
+            get { return _loader ?? (_loader = ResourceLoader.GetForCurrentView("Resources")); }
+        }
+
+        private static ResourceLoader _loader;
+        private static readonly Dictionary<string, string> ResourceCache = new Dictionary<string, string>();
+
+        public static string GetString(string key)
+        {
+            string s;
+            if (ResourceCache.TryGetValue(key, out s))
+            {
+                return s;
+            }
+            else
+            {
+                s = CurrentResourceLoader.GetString(key);
+                ResourceCache[key] = s;
+                return s;
+            }
+        }
+
+    }
+}
